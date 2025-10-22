bd1ded352430528ecce4b2c6c34fe2a69c8ff3ff
lingoudappatil <lingoudap@gmail.com>
2025-10-22 10:29:52 +0530

    id

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
   name: {
     type: String,
     required: true,
